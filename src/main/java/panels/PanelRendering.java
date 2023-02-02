c164dca16297c76fcf0d46bcea6004926af53f0c
TestovMaxim <max1testov@gmail.com>
2023-02-02 11:26:17 +0300

    render statistic

--- src/main/java/panels/PanelRendering.java
+++ src/main/java/panels/PanelRendering.java
@@ -9,6 +9,7 @@ import io.github.humbleui.jwm.Window;
 import io.github.humbleui.skija.Canvas;
 import misc.CoordinateSystem2d;
 import misc.CoordinateSystem2i;
+import misc.Stats;
 import misc.Vector2d;
 
 import java.io.File;
@@ -56,6 +57,7 @@ public class PanelRendering extends GridPanel {
         task = new Task(cs, new ArrayList<>());
         // добавляем в нё 10 случайных
         task.addRandomPoints(10);
+        fpsStats = new Stats();
     }
 
     /**
@@ -92,7 +94,11 @@ public class PanelRendering extends GridPanel {
      */
     @Override
     public void paintImpl(Canvas canvas, CoordinateSystem2i windowCS) {
+        // рисуем задачу
         task.paint(canvas, windowCS);
+        // рисуем статистику фпс
+        fpsStats.paint(canvas, windowCS, FONT12, padding);
+        // рисуем перекрестие, если мышь внутри области рисования этой панели
         if (lastInside && lastMove != null)
             task.paintMouse(canvas, windowCS, FONT12, lastWindowCS.getRelativePos(lastMove));
     }
@@ -136,6 +142,11 @@ public class PanelRendering extends GridPanel {
             PanelLog.error("Не получилось прочитать файл " + path + "\n" + e);
         }
     }
+
+    /**
+     * Статистика fps
+     */
+    private final Stats fpsStats;
 
 
 }
