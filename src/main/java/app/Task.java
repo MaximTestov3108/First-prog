8c5fb425606c1e014979a035c860292e031c58ca
TestovMaxim <max1testov@gmail.com>
2023-02-02 11:19:07 +0300

    scale works

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -184,7 +184,7 @@ public class Task {
                 Point b = points.get(j);
                 // если точки совпадают по положению
                 if (a.pos.equals(b.pos) && !a.pointSet.equals(b.pointSet)) {
-                    if (!crossed.contains(a)){
+                    if (!crossed.contains(a)) {
                         crossed.add(a);
                         crossed.add(b);
                     }
@@ -200,6 +200,7 @@ public class Task {
         // задача решена
         solved = true;
     }
+
     /**
      * Отмена решения задачи
      */
@@ -292,6 +293,25 @@ public class Task {
             }
         }
         canvas.restore();
+    }
+
+    /**
+     * коэффициент колёсика мыши
+     */
+    private static final float WHEEL_SENSITIVE = 0.001f;
+
+    /**
+     * Масштабирование области просмотра задачи
+     *
+     * @param delta  прокрутка колеса
+     * @param center центр масштабирования
+     */
+    public void scale(float delta, Vector2i center) {
+        if (lastWindowCS == null) return;
+        // получаем координаты центра масштабирования в СК задачи
+        Vector2d realCenter = ownCS.getCoords(center, lastWindowCS);
+        // выполняем масштабирование
+        ownCS.scale(1 + delta * WHEEL_SENSITIVE, realCenter);
     }
 
 
