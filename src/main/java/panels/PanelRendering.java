d65b5492220813489cfb3fa9de680b60c9e4d342
TestovMaxim <max1testov@gmail.com>
2023-02-02 11:21:02 +0300

    mouse cursor

--- src/main/java/panels/PanelRendering.java
+++ src/main/java/panels/PanelRendering.java
@@ -15,6 +15,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static app.Fonts.FONT12;
+
 
 /**
  * Панель рисования
@@ -91,6 +93,8 @@ public class PanelRendering extends GridPanel {
     @Override
     public void paintImpl(Canvas canvas, CoordinateSystem2i windowCS) {
         task.paint(canvas, windowCS);
+        if (lastInside && lastMove != null)
+            task.paintMouse(canvas, windowCS, FONT12, lastWindowCS.getRelativePos(lastMove));
     }
 
     /**
